971af2dcc8f82cb812d5a6596dca02fb17668a26
boteandopiola <85791254+boteandopiola@users.noreply.github.com>
2021-06-12 09:30:45 -0300

    Update basicv001.py

--- basicv001.py
+++ basicv001.py
@@ -2,7 +2,7 @@ import requests
 
 def telegram_bot_sendtext(bot_message):
 
-    bot_token = '1876624034:AAHqW2qFgj60NnMHqGjnltVyo5ZsKp1AkKk'
+    bot_token = '1876624034:AAGzIvnkUmp-DriuSPS3ioMeHRFS6PggNSQ'
     bot_chatID = '843423661'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
