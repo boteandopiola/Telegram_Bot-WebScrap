c103becb5243af0da3bb6a2dd7892ff1261e46e0
boteandopiola <85791254+boteandopiola@users.noreply.github.com>
2021-10-09 21:12:30 -0300

    Update basicv001.py

--- basicv001.py
+++ basicv001.py
@@ -1,21 +1,13 @@
-from telebot import TeleBot
-import time
+import telebot
 import urllib
 import schedule
 
 TOKEN = '2094252378:AAHrWLPNeAc6fPriFBxbvDMdSe_JbzLq79I' # Ponemos nuestro Token generado con el @BotFather
 
-def report():
-    tb = telebot.TeleBot('2094252378:AAHrWLPNeAc6fPriFBxbvDMdSe_JbzLq79I')
-    url='http://www.bcra.gov.ar/Pdfs/PublicacionesEstadisticas/infomondiae.pdf'
-    f = open('out.pdf','wb')
-    f.write(urllib.request.urlopen(url).read())
-    f.close()
-    tb.send_document('-703364548', url)
-    tb.send_message('-703364548', 'Te dejo el informe actualizado Rey ;)')
-
-    schedule.every().day.at("21:05").do(report)
-    
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+tb = telebot.TeleBot('2094252378:AAHrWLPNeAc6fPriFBxbvDMdSe_JbzLq79I')
+url='http://www.bcra.gov.ar/Pdfs/PublicacionesEstadisticas/infomondiae.pdf'
+f = open('out.pdf','wb')
+f.write(urllib.request.urlopen(url).read())
+f.close()
+tb.send_document('-703364548', url)
+tb.send_message('-703364548', 'Te dejo el informe actualizado Rey ;)')
