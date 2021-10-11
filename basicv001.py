c8e22e96b3edbb370f93de58882b935755a74a0d
boteandopiola <85791254+boteandopiola@users.noreply.github.com>
2021-10-11 12:25:33 -0300

    Update basicv001.py

--- basicv001.py
+++ basicv001.py
@@ -1,13 +1,19 @@
 import telebot
+import time
 import urllib
 import schedule
 
-TOKEN = '2094252378:AAHrWLPNeAc6fPriFBxbvDMdSe_JbzLq79I' # Ponemos nuestro Token generado con el @BotFather
+TOKEN = '2094252378:AAHrWLPNeAc6fPriFBxbvDMdSe_JbzLq79I'
 
-tb = telebot.TeleBot('2094252378:AAHrWLPNeAc6fPriFBxbvDMdSe_JbzLq79I')
-url='http://www.bcra.gov.ar/Pdfs/PublicacionesEstadisticas/infomondiae.pdf'
-f = open('out.pdf','wb')
-f.write(urllib.request.urlopen(url).read())
-f.close()
-tb.send_document('-703364548', url)
-tb.send_message('-703364548', 'Te dejo el informe actualizado Rey ;)')
+def report():
+    tb = telebot.TeleBot('2094252378:AAHrWLPNeAc6fPriFBxbvDMdSe_JbzLq79I')
+    url='http://www.bcra.gov.ar/Pdfs/PublicacionesEstadisticas/infomondiae.pdf'
+    f = open('out.pdf','wb')
+    f.write(urllib.request.urlopen(url).read())
+    f.close()
+    tb.send_document('843423661', url)
+
+schedule.every().day.at("12:30").do(report)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
